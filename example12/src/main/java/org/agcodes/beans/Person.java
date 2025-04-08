9bff2b02eb427cafae0fd5c175adf4728ec1b8ff
amirag96 <amirag.wasfi@gmail.com>
2025-04-08 13:36:59 +0200

    @Autowire on Setter Method

---
+++ example12/src/main/java/org/agcodes/beans/Person.java
@@ -0,0 +1,35 @@
+package org.agcodes.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+  private String name="Lucy";
+  private Vehicle vehicle;
+
+  public Person() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Vehicle getVehicle() {
+    return vehicle;
+  }
+
+  /*
+   2. Setter Injection:
+    -> Optional dependencies or when you need to change them later.
+   */
+  @Autowired
+  public void setVehicle(Vehicle vehicle) {
+    this.vehicle = vehicle;
+  }
+}
