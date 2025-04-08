f5dade52637f86a5dd0df6dd4c62a709ca8b536d
amirag96 <amirag.wasfi@gmail.com>
2025-04-08 13:46:53 +0200

    @Autowire with Constructor

---
+++ example13/src/main/java/org/agcodes/beans/Vehicle.java
@@ -0,0 +1,29 @@
+package org.agcodes.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+  private String name= "Toyota";
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void printHello(){
+    System.out.println(
+        "Printing Hello from Component Vehicle Bean");
+  }
+
+  @Override
+  public String toString() {
+    return "Vehicle{" +
+        "name='" + name + '\'' +
+        '}';
+  }
+}
