2bdd65d60d93aa455a096011535d01e034f17f52
amirag96 <amirag.wasfi@gmail.com>
2025-04-14 20:31:00 +0200

    @RequestParam in Eazy School Web App

---
+++ example_22/src/main/java/org/agcodes/cookingschool/service/ContactService.java
@@ -0,0 +1,19 @@
+package org.agcodes.cookingschool.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.agcodes.cookingschool.controller.ContactController;
+import org.agcodes.cookingschool.model.Contact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+@Slf4j
+@Service
+public class ContactService {
+//  private static Logger log = LoggerFactory.getLogger(ContactService.class);
+
+  public boolean saveMessageDetails(Contact contact){
+    boolean isSaved= true;
+    log.info(contact.toString());
+    return isSaved;
+  }
+}
