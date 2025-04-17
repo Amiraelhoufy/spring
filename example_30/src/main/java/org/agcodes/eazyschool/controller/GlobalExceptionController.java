39b3f14bb86bb7007b1f2ad8d80460199c17e259
amirag96 <amirag.wasfi@gmail.com>
2025-04-17 14:10:32 +0200

    @ControllerAdvice + @ExceptionHandler for global exception handling

---
+++ example_30/src/main/java/org/agcodes/eazyschool/controller/GlobalExceptionController.java
@@ -0,0 +1,33 @@
+package org.agcodes.eazyschool.controller;
+
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionController {
+
+  // View : which HTML template to render
+  // Model : Model attributes to include error details such as error message
+  @ExceptionHandler
+  public ModelAndView exceptionHandler(Exception exception){
+    ModelAndView errorPage = new ModelAndView();
+    errorPage.setViewName("error"); // Refers to error.html
+    errorPage.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+    errorPage.addObject("errorMsg",exception.getMessage());
+    return errorPage;
+  }
+
+  // Providing cetrain Handling for group of exceptions
+
+/*  @ExceptionHandler({NullPointerException.class,
+  ArrayIndexOutOfBoundsException.class, IOException.class})
+  public ModelAndView handleException(RuntimeException ex){
+    // Handling logic
+  }
+  */
+}
