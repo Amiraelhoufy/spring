d79de9961280d4ebf573bedd92ec9c5d953c9698
amirag96 <amirag.wasfi@gmail.com>
2025-04-23 12:02:21 +0200

    Spring JPA Auditing using CreatedBy,LastModifiedBy,CreatedDate,LastModifiedDate

---
+++ example_36/src/main/java/org/agcodes/eazyschool/controller/HolidaysController.java
@@ -0,0 +1,47 @@
+package org.agcodes.eazyschool.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import org.agcodes.eazyschool.model.Holiday;
+import org.agcodes.eazyschool.repository.HolidaysRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HolidaysController {
+  @Autowired
+  private HolidaysRepository holidaysRepository;
+  @GetMapping("/holidays/{display}")
+  public String displayHolidays(@PathVariable String display, Model model){
+
+    if(null!=display && display.equals("all")){
+      model.addAttribute("festival",true);
+      model.addAttribute("federal",true);
+    }else if(null!=display && display.equals("federal")){
+      model.addAttribute("federal",true);
+    }else if(null!=display && display.equals("festival")){
+      model.addAttribute("festival",true);
+    }
+    Iterable<Holiday> holidays = holidaysRepository.findAll();
+    // convert iterable to list
+    List<Holiday> holidayList = StreamSupport.stream(
+        holidays.spliterator(), false)
+        .collect(
+        Collectors.toList());
+
+    Holiday.Type[] types = Holiday.Type.values();
+
+    for (Holiday.Type type : types) {
+      model.addAttribute(type.toString(),
+          (holidayList.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
+    }
+
+    return "holidays.html";
+
+  }
+
+}
